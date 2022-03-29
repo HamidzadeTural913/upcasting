287af5c47f837c54578d1ff8c67da3757f41dc2d
HamidzadeTural913 <tu76fe6rc@code.edu.az>
2022-03-29 08:53:35 +0400

    Task

---
+++ upcasting/upcasting/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace upcasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MainPrintFile word = new Word();
+            word.Print();
+
+            MainPrintFile excel = new Excel();
+            excel.Print();
+           
+            MainPrintFile pdf = new PDF();
+            pdf.Print();
+        }
+
+
+        class Word : MainPrintFile
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Word fayl elave olundu");
+            }
+               
+                
+        }
+        class Excel:MainPrintFile
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Excel fayl elave olundu");
+            }
+
+        }
+         
+        class PDF : MainPrintFile
+        {
+            public override void Print()
+            {
+                Console.WriteLine("PDF fayl elave olundu");
+            }
+
+        }
+
+        class MainPrintFile:Iprintable
+        {
+            public virtual void Print()
+            {
+         
+            }
+
+           
+          
+        }
+    }
+}
